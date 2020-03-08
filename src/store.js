bb6a0bf8a6ba295d0ca646dbf560f70cefccecc2
Jaak Peldes <jaakpeldes@gmail.com>
2020-03-08 12:45:22 +0800

    fix middleware error

--- src/store.js
+++ src/store.js
@@ -4,6 +4,8 @@ import holidaysReducer from "./features/holidays/holidaysSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
+import thunk from 'redux-thunk';
+
 // Combine reducers
 const reducers = combineReducers({
   holidays: holidaysReducer
@@ -18,7 +20,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 // Export
-let store = configureStore({ reducer: persistedReducer });
+let store = configureStore({ reducer: persistedReducer, middleware: [thunk] });
 let persistor = persistStore(store);
 
 export { store, persistor };
