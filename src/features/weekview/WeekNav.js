91fdc95d3863f136ff4e46f9d727b8aa92b7ef54
Jaak Peldes <jaakpeldes@gmail.com>
2020-03-09 15:40:22 +0800

    more margin for bottom navbar

--- src/features/weekview/WeekNav.js
+++ src/features/weekview/WeekNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import moment from "moment";
 import {
+  Box,
   BottomNavigation,
   BottomNavigationAction,
   Paper
@@ -10,35 +11,38 @@ import CalendarTodayIcon from "@material-ui/icons/Today";
 import ArrowRightIcon from "@material-ui/icons/ArrowForward";
 import ArrowLeftIcon from "@material-ui/icons/ArrowBack";
 
+import styles from './WeekView.module.css';
 
 export function WeekNav({ theMoment, onChangeDate }) {
   return (
-    <Paper>
-      <BottomNavigation
-        value={theMoment}
-        onChange={(event, newValue) => onChangeDate(newValue)}
-        showLabels
-      >
-        <BottomNavigationAction
-          label="Last week"
-          value={moment(theMoment)
-            .subtract(7, "days")
-            .format("YYYY-MM-DD")}
-          icon={<ArrowLeftIcon />}
-        />
-        <BottomNavigationAction
-          label="Today"
-          value={moment().format("YYYY-MM-DD")}
-          icon={<CalendarTodayIcon />}
-        />
-        <BottomNavigationAction
-          label="Next week"
-          value={moment(theMoment)
-            .add(7, "days")
-            .format("YYYY-MM-DD")}
-          icon={<ArrowRightIcon color="inherit" />}
-        />
-      </BottomNavigation>
-    </Paper>
+    <Box className={styles.weekNav}>
+      <Paper>
+        <BottomNavigation
+          value={theMoment}
+          onChange={(event, newValue) => onChangeDate(newValue)}
+          showLabels
+        >
+          <BottomNavigationAction
+            label="Last week"
+            value={moment(theMoment)
+              .subtract(7, "days")
+              .format("YYYY-MM-DD")}
+            icon={<ArrowLeftIcon />}
+          />
+          <BottomNavigationAction
+            label="Today"
+            value={moment().format("YYYY-MM-DD")}
+            icon={<CalendarTodayIcon />}
+          />
+          <BottomNavigationAction
+            label="Next week"
+            value={moment(theMoment)
+              .add(7, "days")
+              .format("YYYY-MM-DD")}
+            icon={<ArrowRightIcon color="inherit" />}
+          />
+        </BottomNavigation>
+      </Paper>
+    </Box>
   );
 }
